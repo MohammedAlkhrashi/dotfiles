b733eb05965951ddfbf24ece1216f7691ac2e334
mk <mkhrashi@sdaia.gov.sa>
2025-10-26 13:59:36 +0300

    update

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -15,15 +15,15 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Set up plugins
 require('lazy').setup {
-  require 'plugins.neotree',
+  -- require 'plugins.neotree',
   require 'plugins.colortheme',
   require 'plugins.bufferline',
   require 'plugins.lualine',
   require 'plugins.treesitter',
   require 'plugins.telescope',
-  require 'plugins.lsp',
+  -- require 'plugins.lsp',
   require 'plugins.autocompletion',
-  require 'plugins.none-ls',
+  -- require 'plugins.none-ls',
   require 'plugins.gitsigns',
   require 'plugins.alpha',
   require 'plugins.indent-blankline',
